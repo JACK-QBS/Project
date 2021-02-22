1d4a85e1960d88be692d34781338e7f5f479a73a
JACK-QBS <2200838299@qq.com>
2021-02-22 20:55:27 +0800

    每一个本地html文件对应一个文档对象

---
+++ 项目2：搜索引擎/src/main/java/org/example/model/DocInfo.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+import java.util.Objects;
+
+/**
+ * 每一个本地html文件对应一个文档对象
+ */
+public class DocInfo {
+    private Integer id;//类似数据库主键（识别不同文档）
+    private String title;//标题：html文件名作为标题
+    private String url; //oracle官网api文档下html的url
+    private String content;//网页正文：<标签>内容</标签>，内容为正文
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DocInfo docInfo = (DocInfo) o;
+        return Objects.equals(id, docInfo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "DocInfo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
