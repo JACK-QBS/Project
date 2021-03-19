a575fddf6b0f32627a3b7f559fcfec4f2e72ee2c
JACK-QBS <2200838299@qq.com>
2021-03-19 09:40:44 +0800

    定义 Cookie 类的模板方法

---
+++ 项目3：简易服务器/src/com/standard/http/Cookie.java
@@ -0,0 +1,23 @@
+package com.standard.http;
+
+public class Cookie {
+    private String name;
+    private String value;
+
+    public Cookie(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
