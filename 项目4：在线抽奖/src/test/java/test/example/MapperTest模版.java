846cfd0383bdb53510d297b742bde4717469b4c0
JACK-QBS <2200838299@qq.com>
2021-05-31 21:14:02 +0800

    MapperTest模版

---
+++ 项目4：在线抽奖/src/test/java/test/example/MapperTest模版.java
@@ -0,0 +1,18 @@
+package test.example;
+
+import org.example.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+//指定为Spring环境中的单元测试
+@RunWith(SpringRunner.class)
+//指定为SpringBoot环境的单元测试，Application为启动类
+@SpringBootTest(classes = Application.class)
+//使用事务，在SpringBoot的单元测试中会自动回滚
+@Transactional
+public class MapperTest模版 {
+
+
+}
