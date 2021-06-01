744a8619644d2609642a48813f61536f4cf1362a
JACK-QBS <2200838299@qq.com>
2021-06-01 08:20:50 +0800

    所有 mapper 的父接口

---
+++ 项目4：在线抽奖/src/main/java/org/example/base/BaseMapper.java
@@ -0,0 +1,19 @@
+package org.example.base;
+
+/**
+ * 所有 mapper 父接口
+ */
+public interface BaseMapper<T> {
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(T record);
+
+    int insertSelective(T record);
+
+    T selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(T record);
+
+    int updateByPrimaryKey(T record);
+}
